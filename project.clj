6121f351d34cdcf60d357efb72297ced95e86674
Myles Megyesi <myles.megyesi@gmail.com>
2011-11-02 12:19:20 -0500

    Second Commit

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject tic-tac-toe "0.1"
+(defproject tictactoe "0.1"
   :description "Tic-Tac-Toe in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[speclj "1.2.0"]]
+	:dev-dependencies [[speclj "1.2.0"]]
 	:test-path "spec/")
